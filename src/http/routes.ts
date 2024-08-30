a07666c2f8d74dcc0b5f5330b4f7f75abf2efe13
Gabriel <gabrielcostatoledo@outlook.com>
2024-08-30 15:03:36 -0300

    feat: add search route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { upload } from './upload'
 import { confirm } from './confirm'
+import { search } from './search'
 
 export const routes = async (app: FastifyInstance) => {
+  app.get('/:customer_code/list', search)
+
   app.post('/upload', upload)
 
   app.post('/confirm', confirm)
