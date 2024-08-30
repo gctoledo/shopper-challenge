c4c56cf3e7d82c2e9d54b1556d4e2123ed3cc453
Gabriel <gabrielcostatoledo@outlook.com>
2024-08-30 13:55:10 -0300

    feat: add confirm route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { upload } from './upload'
+import { confirm } from './confirm'
 
 export const routes = async (app: FastifyInstance) => {
   app.post('/upload', upload)
+
+  app.post('/confirm', confirm)
 }
